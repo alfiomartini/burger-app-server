d717915adc7e73f7730cf5c0e8c5e9613a32c616
Alfio Martini <alfio.martini@gmail.com>
2024-02-26 17:57:17 -0300

    add mysql queries to get routes - ingredients

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "burger_app",
+});
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Return all ingredients");
+router.get("/", async (req, res) => {
+  try {
+    const [results] = await connection.query("select * from ingredient");
+    console.log("results", results);
+    res.status(200).json(results);
+  } catch (error) {
+    console.log("get all ingredients", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
 router.post("/", (req, res) => {
@@ -11,9 +26,19 @@ router.post("/", (req, res) => {
   res.json({ name, quantity, description });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  res.json({ id });
+  try {
+    const [results] = await connection.query(
+      "select * from ingredient where ing_id = ?",
+      [id]
+    );
+    console.log("results", results);
+    res.status(200).json(results[0]);
+  } catch (error) {
+    console.log("get all ingredients", error);
+    res.status(500).send("Internal server error");
+  }
 });
 
 router.delete("/:id", (req, res) => {
