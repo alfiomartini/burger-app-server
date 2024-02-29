a2f0f1d148b41f2ebe5bb938f3ef6b62af7b8473
Alfio Martini <alfio.martini@gmail.com>
2024-02-29 00:56:37 -0300

    fix script for db burger_app

--- database/schema/script.sql
+++ database/schema/script.sql
@@ -1,7 +1,8 @@
-DROP DATABASE burger_app;
-CREATE DATABASE burger_app;
+DROP DATABASE IF EXISTS burger_app ;
+CREATE DATABASE IF NOT EXISTS burger_app ;
+USE burger_app;
 
-- Tables
+-- Tables
 CREATE TABLE ingredient(
   ing_id INT AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
@@ -44,64 +45,64 @@ CREATE TABLE purchase(
 INSERT INTO ingredient
   (name, quantity, description)
 VALUES
-  ('pickles', 300, 'grams'),
-  ('onion', 500, 'grams'),
-  ('bun', 30, 'units'),
-  ('tomato', 500, 'grams'),
-  ('ketchup', 500, 'grams'),
-  ('lettuce', 500, 'grams'),
-  ('egg', 20, 'units'),
-  ('beef', '500', 'grams'),
-  ('cheese', 250, 'grams'),
-  ('chicken', 500, 'grams'),
-  ('fish', 500, 'grams'),
-  ('fish sauce', 200, 'milliliters'),
-  ('mustard', 300, 'grams'),
-  ('mayo', 300, 'grams');
+  ('pickles', 300, 'grams'), -- 1
+  ('onion', 500, 'grams'),  -- 2
+  ('bun', 30, 'units'),  -- 3
+  ('tomato', 500, 'grams'), -- 4
+  ('ketchup', 500, 'grams'),  -- 5
+  ('lettuce', 500, 'grams'),  -- 6
+  ('egg', 20, 'units'),  -- 7
+  ('beef', '500', 'grams'),  -- 8
+  ('cheese', 250, 'grams'),  -- 9
+  ('chicken', 500, 'grams'),  -- 10
+  ('fish', 500, 'grams'), -- 11
+  ('fish sauce', 200, 'milliliters'), -- 12
+  ('mustard', 300, 'grams'), -- 13
+  ('mayo', 300, 'grams'); -- 14
 
   INSERT INTO burger 
      (name, description)
   VALUES
-     ('standard burger', 'a mouth watering honest beef burger'),
-     ('chicken burger', 'a chicken sandwich consisting of boneless, skinless chicken breast, served between slices of bread'),
-     ('fish burger', 'a homemade fish burger with a crunchy crumb, healthy and packed with flavour');
+     ('standard burger', 'a mouth watering honest beef burger'), -- 1
+     ('chicken burger', 'a chicken sandwich consisting of boneless, skinless chicken breast, served between slices of bread'), -- 2
+     ('fish burger', 'a homemade fish burger with a crunchy crumb, healthy and packed with flavour'); -- 3
 
 INSERT INTO requires 
    (ing_id, burger_id, ing_quantity) -- standard burger
 VALUES
-   (8,7,150), -- beef
-   (9,7,20),  -- cheese
-   (16,7,20), -- mustard
-   (1,7,30),  -- pickles
-   (6,7,20),  -- lettuce
-   (3,7,2);   -- bun
+   (8,1,150), -- beef
+   (9,1,20),  -- cheese
+   (13,1,20), -- mustard
+   (1,1,30),  -- pickles
+   (6,1,20),  -- lettuce
+   (3,1,2);   -- bun
 
 
 INSERT INTO requires 
    (ing_id, burger_id, ing_quantity) -- chicken burger
 VALUES
-   (3,8,2), -- bun
-   (10,8,150),  -- chicken
-   (6,8,20), -- lettuce
-   (17,8,20),  -- mayo
-   (9,8,20),  -- cheese
-   (4,8,40);   -- tomato
+   (3,2,2), -- bun
+   (10,2,150),  -- chicken
+   (6,2,20), -- lettuce
+   (14,2,20),  -- mayo
+   (9,2,20),  -- cheese
+   (4,2,40);   -- tomato
 
 
 INSERT INTO requires 
    (ing_id, burger_id, ing_quantity) -- fish burger
 VALUES
-   (3,9,2), -- bun
-   (11,9,150),  -- fish
-   (6,9,20), -- lettuce
-   (17,9,30),  -- mayo
-   (4,9,20),  -- tomato
-   (15,9,50);   -- fish sauce
+   (3,3,2), -- bun
+   (11,3,150),  -- fish
+   (6,3,20), -- lettuce
+   (14,3,30),  -- mayo
+   (4,3,20),  -- tomato
+   (12,3,50);   -- fish sauce
 
 INSERT INTO purchase
     (burger_id, customer_name)
 VALUES
-    (7, 'Mary Williams'),
-    (8, 'John Taylor'),
-    (9, 'Simon Miller'),
-    (7, 'Suzy Jones');
+    (1, 'Mary Williams'),
+    (2, 'John Taylor'),
+    (3, 'Simon Miller'),
+    (1, 'Suzy Jones');
