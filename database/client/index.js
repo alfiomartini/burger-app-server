bd6d6bcaa4d66241b83928fa1ae8a5a8bfd84983
Alfio Martini <alfio.martini@gmail.com>
2024-03-01 01:26:40 -0300

    dockerize only the db - use pools

--- database/client/index.js
+++ database/client/index.js
@@ -1,12 +1,28 @@
-import express from "express";
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-const connection = await mysql.createConnection({
-  host: "localhost",
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DB,
   password: process.env.MYSQL_PWD,
-  database: "burger_app",
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
-export { connection };
+// const connection = await mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PWD,
+//   database: process.env.MYSQL_DB,
+//   port: process.env.MYSQL_PORT,
+// });
+
+export { pool as connection };
