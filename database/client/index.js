54bb8d6e08e7455262de6440ef1850dce870dd7d
Alfio Martini <alfio.martini@gmail.com>
2024-03-01 04:02:52 -0300

    remove comments in the client pool

--- database/client/index.js
+++ database/client/index.js
@@ -17,12 +17,4 @@ const pool = mysql.createPool({
   keepAliveInitialDelay: 0,
 });
 
-// const connection = await mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PWD,
-//   database: process.env.MYSQL_DB,
-//   port: process.env.MYSQL_PORT,
-// });
-
 export { pool as connection };
