fb8f3702e7bc13bc5f8687192395630b5a2bac2f
Alfio Martini <alfio.martini@gmail.com>
2024-03-01 13:51:19 -0300

    fix script to guarantee persistence in the db volumes

--- database/schema/script.sql
+++ database/schema/script.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS burger_app ;
+-- DROP DATABASE IF EXISTS burger_app ;
 CREATE DATABASE IF NOT EXISTS burger_app ;
 USE burger_app;
 
 -- Tables
-CREATE TABLE ingredient(
+CREATE TABLE IF NOT exists ingredient(
   ing_id INT AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   quantity INT NOT NULL,
@@ -13,14 +13,14 @@ CREATE TABLE ingredient(
 
 
 
-CREATE TABLE burger(
+CREATE TABLE IF NOT EXISTS burger(
   burger_id INT AUTO_INCREMENT,
   name VARCHAR(20) NOT NULL,
   description VARCHAR(250) NOT NULL,
   PRIMARY KEY (burger_id)
 );
 
-CREATE TABLE requires(
+CREATE TABLE IF NOT EXISTS requires(
   ing_id INT,
   burger_id INT,
   ing_quantity INT NOT NULL,
@@ -29,10 +29,9 @@ CREATE TABLE requires(
   FOREIGN KEY(burger_id) REFERENCES burger(burger_id) ON DELETE CASCADE
 );
 
--- this next table only makes sense if while
--- we don't include 'Users' in the model
+-- this next table only makes sense while we don't include 'Users' in the model
 
-CREATE TABLE purchase(
+CREATE TABLE IF NOT EXISTS purchase(
   purchase_id INT AUTO_INCREMENT,
   burger_id INT,
   customer_name VARCHAR(20) NOT NULL,
@@ -42,32 +41,37 @@ CREATE TABLE purchase(
 );
 
 -- insertion of data
-INSERT INTO ingredient
-  (name, quantity, description)
+
+
+ 
+INSERT IGNORE INTO ingredient 
+   (name, quantity, description)
+   VALUES
+      ('pickles', 300, 'grams'), -- 1
+      ('onion', 500, 'grams'),  -- 2
+      ('bun', 30, 'units'),  -- 3
+      ('tomato', 500, 'grams'), -- 4
+      ('ketchup', 500, 'grams'),  -- 5
+      ('lettuce', 500, 'grams'),  -- 6
+      ('egg', 20, 'units'),  -- 7
+      ('beef', '500', 'grams'),  -- 8
+      ('cheese', 250, 'grams'),  -- 9
+      ('chicken', 500, 'grams'),  -- 10
+      ('fish', 500, 'grams'), -- 11
+      ('fish sauce', 200, 'ml'), -- 12
+      ('mustard', 300, 'grams'), -- 13
+      ('mayo', 300, 'grams'); -- 14
+
+
+INSERT IGNORE INTO burger 
+   (name, description)
 VALUES
-  ('pickles', 300, 'grams'), -- 1
-  ('onion', 500, 'grams'),  -- 2
-  ('bun', 30, 'units'),  -- 3
-  ('tomato', 500, 'grams'), -- 4
-  ('ketchup', 500, 'grams'),  -- 5
-  ('lettuce', 500, 'grams'),  -- 6
-  ('egg', 20, 'units'),  -- 7
-  ('beef', '500', 'grams'),  -- 8
-  ('cheese', 250, 'grams'),  -- 9
-  ('chicken', 500, 'grams'),  -- 10
-  ('fish', 500, 'grams'), -- 11
-  ('fish sauce', 200, 'ml'), -- 12
-  ('mustard', 300, 'grams'), -- 13
-  ('mayo', 300, 'grams'); -- 14
+   ('standard burger', 'a mouth watering honest beef burger'), -- 1
+   ('chicken burger', 'a chicken sandwich consisting of boneless, skinless chicken breast, served between slices of bread'), -- 2
+   ('fish burger', 'a homemade fish burger with a crunchy crumb, healthy and packed with flavour'); -- 3
 
-  INSERT INTO burger 
-     (name, description)
-  VALUES
-     ('standard burger', 'a mouth watering honest beef burger'), -- 1
-     ('chicken burger', 'a chicken sandwich consisting of boneless, skinless chicken breast, served between slices of bread'), -- 2
-     ('fish burger', 'a homemade fish burger with a crunchy crumb, healthy and packed with flavour'); -- 3
 
-INSERT INTO requires 
+INSERT IGNORE INTO requires 
    (ing_id, burger_id, ing_quantity) -- standard burger
 VALUES
    (8,1,150), -- beef
@@ -78,7 +82,8 @@ VALUES
    (3,1,2);   -- bun
 
 
-INSERT INTO requires 
+ 
+INSERT IGNORE INTO requires 
    (ing_id, burger_id, ing_quantity) -- chicken burger
 VALUES
    (3,2,2), -- bun
@@ -89,7 +94,8 @@ VALUES
    (4,2,40);   -- tomato
 
 
-INSERT INTO requires 
+ 
+INSERT IGNORE INTO requires 
    (ing_id, burger_id, ing_quantity) -- fish burger
 VALUES
    (3,3,2), -- bun
@@ -99,10 +105,12 @@ VALUES
    (4,3,20),  -- tomato
    (12,3,50);   -- fish sauce
 
-INSERT INTO purchase
-    (burger_id, customer_name)
+
+ 
+INSERT IGNORE INTO purchase
+   (burger_id, customer_name)
 VALUES
-    (1, 'Mary Williams'),
-    (2, 'John Taylor'),
-    (3, 'Simon Miller'),
-    (1, 'Suzy Jones');
+   (1, 'Mary Williams'),
+   (2, 'John Taylor'),
+   (3, 'Simon Miller'),
+   (1, 'Suzy Jones');
